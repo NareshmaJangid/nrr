b0c7bf191e059a962f6606cba91619f1bdd91efc
NareshmaJangid <48437295+NareshmaJangid@users.noreply.github.com>
2019-04-01 15:58:57 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter tha value",a);
+    scanf("%d",&a);
+    if(a>0)
+    printf("A is a positive integer");
+    else if(a=0)
+    printf("A is zero");
+    else 
+    printf("A is a negative integer");
+    
+}
